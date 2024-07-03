6567279ab1a2ba12c693cc7e63c409d9d8f8dfef
mikey <bonelessone@protonmail.com>
2024-07-02 20:34:00 -0700

    created microchip studio proj

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * mafd-atmega-firmware.cpp
+ *
+ * Created: 7/2/2024 7:47:04 PM
+ * Author : mikey
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
